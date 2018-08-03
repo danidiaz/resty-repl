df1261bc9d2d563e3eca5ce1259db3ce23edbdba
daniel <daniel@bogusemailserver.com>
2018-08-03 16:41:55 +0000

    repl function

--- library/RestyRepl.hs
+++ library/RestyRepl.hs
@@ -1,5 +1,51 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ApplicativeDo #-}
 module RestyRepl (
+        repl
     ) where
 
+import           Data.Sequence
+import           Data.Monoid
+import           Data.Text
+import           Data.Text.Encoding (encodeUtf8)
+import qualified Data.Text.Lazy
+import           Control.Monad
+import           Control.Monad.STM
+import           Control.Concurrent.STM.TVar
+import           Control.Concurrent.STM.TQueue
+import           System.IO
+import           System.Process.Streaming ( proc
+                                          , piped
+                                          , executeInteractive
+                                          , foldOut 
+                                          , withConsumer 
+                                          , feedProducer)
+import           System.Process.Streaming.Text ( asUtf8x
+                                               , asFoldedLines
+                                               )
+import           Pipes
+import qualified Pipes.Prelude
 
+type History = Seq Data.Text.Lazy.Text
+
+repl :: FilePath -> [String] -> IO (Text -> IO (), IO History, IO ())
+repl executable arguments = do
+    inputQueue <- atomically $ newTQueue @Text
+    historyRef <- atomically $ newTVar @History mempty
+    let execution = 
+            executeInteractive 
+            (piped (proc executable arguments))
+            (feedProducer (producer >-> Pipes.Prelude.map encodeUtf8)
+             *> 
+             foldOut (asUtf8x (asFoldedLines writeToHistory)))
+        producer = 
+            forever (do text <- liftIO . atomically $ readTQueue inputQueue
+                        yield text)
+        writeToHistory = 
+            withConsumer $ 
+            forever (do line <- await
+                        liftIO . atomically $ modifyTVar' historyRef (|> line))
+    pure $ (,,) (\text -> atomically (writeTQueue inputQueue text)) 
+                (atomically $ readTVar historyRef)
+                execution
 
